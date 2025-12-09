baf218707b28283292ea1b05cc6444dd1d900612
fagleyws <fagleyws@jacks.sfasu.edu>
2025-12-08 20:33:35 -0600

    Removed useless boolean

--- src/main/java/org/gooseapple/game/Game.java
+++ src/main/java/org/gooseapple/game/Game.java
@@ -318,10 +318,8 @@ public class Game extends Level {
             event.getGraphicsContext().fillText("GAME OVER, YOU LOST THE TRAIN", (event.getScreenSize().getX() / 2) - 100, 200);
             event.getGraphicsContext().restore();
             long currentTime = System.currentTimeMillis();
-            boolean endgame = false;
             if (currentTime - System.currentTimeMillis() > 5000) {
                 System.exit(0);
-                endgame = true;
             }
         }
     }
