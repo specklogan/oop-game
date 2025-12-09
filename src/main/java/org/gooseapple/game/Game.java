c6e731acb50bf9d3cf061224973d735f75f8640e
SunnySoulz <hkb4jesus@yahoo.com>
2025-12-08 20:32:39 -0600

    null edits

--- src/main/java/org/gooseapple/game/Game.java
+++ src/main/java/org/gooseapple/game/Game.java
@@ -318,11 +318,11 @@ public class Game extends Level {
             event.getGraphicsContext().fillText("GAME OVER, YOU LOST THE TRAIN", (event.getScreenSize().getX() / 2) - 100, 200);
             event.getGraphicsContext().restore();
             long currentTime = System.currentTimeMillis();
-            boolean endgame = false;
-            while(!endgame){
+            boolean endgame = true;
+            while(endgame){
                 if (currentTime - System.currentTimeMillis() > 5000) {
                     System.exit(0);
-                    endgame = true;
+                    endgame = false;
                 }
             }
         }
